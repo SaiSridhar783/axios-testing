8b7de49a4b25509d9e6099bf27f4865374b40f44
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-03-15 19:49:03 +0530

    React Router

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Sai'
+        author: 'Sai',
+        submitted: false
+    }
+
+    componentDidMount () {
+        console.log(this.props);
     }
 
     postDataHandler = () => {
@@ -15,16 +21,20 @@ class NewPost extends Component {
             body: this.state.content,
             author: this.state.author
         };
-
-        axios.post("/posts", data)
-            .then((response) => {
+        axios.post('/posts', data)
+            .then(response => {
                 console.log(response);
+                this.props.history.push('/posts')
+                //this.props.history.replace('/posts')
+                //this.setState({submitted: true})
             });
-    };
+    }
 
     render () {
+        let redirect = this.state.submitted ? <Redirect to="/posts" /> : null;
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
--- src/index.js
+++ src/index.js
@@ -17,19 +17,5 @@ axios.interceptors.request.use(request => {
     return Promise.reject(error);
 });
 
-const options = {
-    "headers": {
-        "accept": "*/*",
-        "api_key": "n^h[+}K'rr:)<jQ^#*4#k>MvCnIC,,nPv'B~H\\$7d^s]bj#;l1V'{Y,lQj6FsV**",
-        "Content-Type": "application/json"
-    },
-    "data": {
-        "{\"args\":{\"to\":\"919247336979@c.us\",\"content\":\"This is Sparta\"}}": ""
-    },
-    "mode": "cors"
-}
-
-axios.post("https://wpbot42.herokuapp.com/sendText",options).then((response) => console.log(response));
-
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
